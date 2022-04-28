af780ad15bb5308e690f62de0858a3bd15e1e30d
Felipe Alves <felipe.c@compasso.com.br>
2022-04-28 09:51:25 -0300

    chore: initial commit

--- FirsApp/ViewController.swift
+++ FirsApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var user: UITextField!
+    
+    
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func login(_ sender: Any) {
+        
+        if(user.text == "felipe" && password.text == "felipe"){
+            print("Deu bom")
+            performSegue(withIdentifier: "userScreen", sender: self)
+         }
+        else{
+            print("deu ruim")
+        }
+    }
 }
 
